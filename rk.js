f05c1b5f4365974c24feb4c16f09fbc062cdb116
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-05 15:54:37 +0800

    Update rk.js

--- rk.js
+++ rk.js
@@ -2,15 +2,16 @@ var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
 obj = {
-  "upgradable" : false,
-  "discounted_price" : 36100,
-  "discount" : 400,
-  "discount_description" : "限时抵扣￥4",
-  "user_id" : "ehhekn",
-  "is_bigmember" : true,
-  "purchase_button_url" : "https://media-image1.baydn.com/storage_media_image/cmuatp/a9b7c106666ab232a39066112394dc97.b0ce6df5ae1509c729a9e0a37a874bd8.png"
+  "message" : "SUCCESS",
+  "data" : {
+    "vip" : true,
+    "sign_status_upgrade" : false,
+    "vip_expired_days" : 0,
+    "feedback" : true,
+    "img_url" : "https://img10.360buyimg.com/ebookadmin/jfs/t1/104697/37/15969/2383/5e780ef6E1fe37360/a32f18df884cb306.png.dpg",
+    "vip_remain_days" : 365,
+  "result_code" : 0
 }
-
 
 body = JSON.stringify(obj); // 重新打包回json字符串
 $done(body); // 结束修改
