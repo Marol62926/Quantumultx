5b652ca07c50cf3bf34f7dda321c06b7ff90b5c8
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2023-05-19 12:08:07 +0800

    Create ai4.js

---
+++ ai4/ai4.js
@@ -0,0 +1,28 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.accounts.default.account.has_previously_paid_subscription = true
+obj.accounts.default.account.processor = {
+          "a001" : {
+            "has_customer_object" : false
+          },
+          "b001" : {
+            "has_transaction_history" : false
+          }
+        }
+
+obj.accounts.default.entitlement = {
+        "expires_at" : "2029-05-26T05:05:04Z",
+        "subscription_id" : null,
+        "subscription_plan" : "chatgptplusplan",
+        "has_active_subscription" : true
+      },
+  
+obj.accounts.default.last_active_subscription = {
+        "subscription_id" : null,
+        "will_renew" : true,
+        "purchase_origin_platform" : "openai"
+      }
+
+body = JSON.stringify(obj);
+$done({body});
